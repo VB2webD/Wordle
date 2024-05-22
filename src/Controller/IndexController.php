6686fe01dee15d2836b63921feadc0899a34bfd4
Volker Bransky <vbransky@googlemail.com>
2024-05-22 18:47:25 +0200

    Run initial Setup & add Index Controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 class IndexController
 {
-
+#[Route('/')]
+    public function index()
+    {
+        return new Response('Hello World');
+    }
 }
